4640999f3474c77c0471db852cf0c7275221630b
Gibson Smiley <gibsonsmiley@gmail.com>
2016-03-01 21:47:30 -0700

    UserController

--- Timeline/UserController.swift
+++ Timeline/UserController.swift
@@ -29,19 +29,33 @@ class UserController {
     }
     
     static func userForIdentifier(identifier: String, completion: (user: User?) -> Void) {
-        
+        FirebaseController.dataAtEndpoint("/posts/\(identifier)") { (data) -> Void in
+            if let data = data as? [String: AnyObject] {
+                let user = User(json: data, identifier: identifier)
+                completion(user: user)
+            } else {
+                completion(user: nil)
+            }
+        }
     }
     
-    static func fetchAllUsers(completion: (user: [User]) -> Void) {
-        
+    static func fetchAllUsers(completion: (users: [User]) -> Void) {
+        FirebaseController.base.childByAppendingPath("users").observeSingleEventOfType(.Value, withBlock: { (snapshot) -> Void in
+            if let userDictionaries = snapshot.value as? [String: AnyObject] {
+                let users = userDictionaries.flatMap({User(json: $0.1 as! [String: AnyObject], identifier: $0.0)})
+                completion(users: users)
+            }
+        })
     }
     
     static func followUser(user: User, completion: (success: Bool) -> Void) {
-        
+        FirebaseController.base.childByAppendingPath("/users/\(currentUser.identifier)/follows/\(user.identifier!)").setValue(true)
+        completion(success: true)
     }
     
     static func unfollowUser(user: User, completion: (success: Bool) -> Void) {
-        
+        FirebaseController.base.childByAppendingPath("users/\(currentUser.identifier)/follows/\(user.identifier)").removeValue()
+        completion(success: true)
     }
     
     static func userFollowsUser(user1: User, user2: User, completion: (follows: Bool) -> Void) {
@@ -57,7 +71,13 @@ class UserController {
     }
     
     static func createUser(email: String, username: String, password: String, bio: String?, url: String?, completion: (sucess: Bool, user: User?) -> Void) {
-        
+        FirebaseController.base.createUser(email, password: password) { (error, results) -> Void in
+            if let error = error {
+                print("\(error.localizedDescription)")
+            } else {
+                let uid = results["uid"]
+            }
+        }
     }
     
     static func updateUser(user: User, username: String, bio: String?, url: String?, completion: (success: Bool, user: User?) -> Void) {
